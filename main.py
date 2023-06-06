949f85b00d9639ec656dc89700556f264fb221a5
Carlos Aluizio G. Cavalcanti Filho <carlos.cavalcanti@ferreiracosta.local>
2023-06-06 16:18:13 -0300

    Added Bootstrap to blog

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    blog_url = 'https://api.npoint.io/aa8c5caf93bde392d45b'
     response = requests.get(blog_url)
     all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
 @app.route('/post/blog_<num>')
 def post(num):
-    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    blog_url = 'https://api.npoint.io/aa8c5caf93bde392d45b'
     response = requests.get(blog_url)
     all_posts = response.json()
     post = all_posts[int(num)-1]
     return render_template("post.html", post=post)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
