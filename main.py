7d1b27e7224064885704594af81c14d5719f966b
Carlos <carlosagcf@outlook.com>
2023-06-07 11:22:42 -0300

    Made form be sent by email.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
+OWN_EMAIL    = #'YOUR OWN EMAIL ADDRESS'
+OWN_PASSWORD = #'YOUR EMAIL ADDRESS PASSWORD'
 
 app = Flask(__name__)
 
@@ -23,9 +26,29 @@ def post(num):
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+# @app.route('/form_entry', methods=['post'])
+# def form_entry():
+#     name = request.form['name']
+#     email = request.form['email']
+#     phone = request.form['phone']
+#     message = request.form['message']
+#     return render_template("form-entry.html", name=name, email=email, phone=phone, message=message)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(OWN_EMAIL, OWN_PASSWORD)
+    connection.sendmail(OWN_EMAIL, 'carlosagcf@outlook.com', email_message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
